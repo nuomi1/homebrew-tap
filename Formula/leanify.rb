00f12829b98c93ea29a3df8a53097f1fe90b1f77
nuomi1 <nuomi1@qq.com>
2022-02-19 00:05:50 +0800

    refactor: use a tar.gz file

--- Formula/leanify.rb
+++ Formula/leanify.rb
@@ -14,16 +14,16 @@ class Leanify < Formula
   test do
     require "open-uri"
 
-    image_filepath = testpath / "image.png"
+    filepath = testpath / "leanify.tar.gz"
 
-    open("https://httpbin.org/image/png") do |image|
-      File.binwrite(image_filepath, image.read)
+    open("https://github.com/JayXon/Leanify/archive/v0.4.3.tar.gz") do |file|
+      File.binwrite(filepath, file.read)
     end
 
-    system "#{bin}/leanify", image_filepath.to_s
+    system "#{bin}/leanify", filepath.to_s
 
-    image_sha256 = Digest::SHA256.file(image_filepath).to_s
+    file_sha256 = Digest::SHA256.file(filepath).to_s
 
-    assert_match "a5caf07fb8e55709f3f8117360a4689c5a8a48f20f296a7f471f6d7a4cea4e8a", image_sha256
+    assert_match "e31544d1d81712addfb99b82a9f19c27ee58aa080ade6d61485f325c2ff04df6", file_sha256
   end
 end
